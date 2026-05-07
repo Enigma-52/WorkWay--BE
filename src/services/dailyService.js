a57a19568d18adc3aaf50590f603aadee8a4786a
Rohit Singh <rohit.singh.33521@gmail.com>
2026-05-07 15:22:35 +0530

    add basic initial job fetch flow for workable

--- src/services/dailyService.js
+++ src/services/dailyService.js
@@ -61,101 +61,6 @@ export async function insertGreenhouseJobsDaily() {
   return { success: 'true' };
 }
 
-// export async function processMissingForCompany(missingJobIds, company) {
-//   const jobsToInsert = [];
-//   for (const jobId of missingJobIds) {
-//     const jobUrl = `${baseGreenhouseUrl}${company.namespace.toLowerCase()}/jobs/${jobId}`;
-//     const jobDetails = await fetchWithRetry(jobUrl);
-//     const sections = await parseGreenhouseJobDescription(jobDetails.content);
-//     const sectionText = sections.map((s) => [s.heading, ...(s.content || [])].join(' ')).join('\n');
-//     const skills = matchSkillsInText(sectionText);
-
-//     const jobSlugRaw = company.slug + '-' + jobDetails.title + '-' + jobDetails.id;
-//     const jobSlug = jobSlugRaw
-//       .toLowerCase()
-//       .replace(/[^a-z0-9]+/g, '-')
-//       .replace(/^-+|-+$/g, '');
-
-//     const [experience_level, employment_type, domain] = await Promise.all([
-//       getExperienceLevel(jobDetails.title),
-//       getEmploymentType(jobDetails.title),
-//       getJobDomain(jobDetails.title),
-//     ]);
-
-//     const dbRow = {
-//       company: company.name,
-//       company_id: company.id,
-//       slug: jobSlug,
-//       platform: 'greenhouse',
-//       job_id: jobDetails.id,
-//       title: jobDetails.title,
-//       url: jobDetails.absolute_url,
-//       description: JSON.stringify(sections),
-//       experience_level,
-//       employment_type,
-//       domain,
-//       location: jobDetails.location ? jobDetails.location.name : 'Worldwide',
-//       skills: JSON.stringify(skills),
-//       updated_at: new Date().toISOString(),
-//     };
-//     jobsToInsert.push(dbRow);
-//   }
-//   const multiRowsColValuesList = jobsToInsert.map((job) => [
-//     job.company,
-//     job.company_id,
-//     job.slug,
-//     job.platform,
-//     job.job_id,
-//     job.title,
-//     job.url,
-//     job.description,
-//     job.experience_level,
-//     job.employment_type,
-//     job.domain,
-//     job.location,
-//     job.skills,
-//     job.updated_at,
-//   ]);
-
-//   try {
-//     await defaultPgDao.insertOrUpdateMultipleObjs({
-//       tableName: 'jobs',
-//       columnNames: [
-//         'company',
-//         'company_id',
-//         'slug',
-//         'platform',
-//         'job_id',
-//         'title',
-//         'url',
-//         'description',
-//         'experience_level',
-//         'employment_type',
-//         'domain',
-//         'location',
-//         'skills',
-//         'updated_at',
-//       ],
-//       multiRowsColValuesList,
-//       updateColumnNames: [
-//         'title',
-//         'url',
-//         'description',
-//         'experience_level',
-//         'employment_type',
-//         'domain',
-//         'location',
-//         'skills',
-//         'updated_at',
-//       ],
-//       conflictColumns: ['slug'],
-//       returningCol: 'id',
-//     });
-//   } catch (error) {
-//     console.log('oopsie');
-//   }
-// }
-
 export async function processMissingForCompany(missingJobIds, company) {
   if (!missingJobIds.length) return;
 
@@ -277,4 +182,35 @@ const baseWorkableJobsUrl = 'https://apply.workable.com/api/v3/accounts/{company
 const baseWorkableJobDetailUrl = 'https://apply.workable.com/api/v2/accounts/{company}/jobs/{job_id}'
 
 export async function insertWorkableJobsDaily() {
+  const companies = await defaultPgDao.getAllRows({
+    tableName: 'companies',
+    where: "platform = 'workable'",
+    orderBy : 'id ASC',
+  });
+  const c = companies.length;
+  let t = 0;
+  const jobs = [];
+  for (const company of companies) {
+    const getJobsForCompanyFromDB = await defaultPgDao.getAllRowsForChat({
+      tableName: 'jobs',
+      columns: ['job_id'],
+      where: `company_id = ${company.id}`,
+    });
+    const jobIdsFromDB = new Set(getJobsForCompanyFromDB.map((row) => row.job_id));
+    const result = await getWorkableJobs(company.namespace);
+    const apiJobIds = result.jobs.map((job) => String(job.id));
+    const missingJobIds = apiJobIds.filter((id) => !jobIdsFromDB.has(id));
+    t += 1;
+    if (missingJobIds.length == 0) continue;
+    await processMissingWorkableJobsForCompany(missingJobIds, company);
+    console.log('Inserted ', missingJobIds.length, ' jobs for ', company.namespace, ' ', t, '/', c);
+  }
+  return { success: 'true' };
+}
+
+export async function getWorkableJobs(company) {
+}
+
+export async function processMissingWorkableJobsForCompany(missingJobIds, company) {
+
 }
